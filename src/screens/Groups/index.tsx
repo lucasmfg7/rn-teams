653879e0f48299682aa699d4609bd9418e54b58d
Lucas Fernandes <lucas_matheus22@hotmail.com>
2023-09-20 11:31:45 -0300

    Organizando e executando o projeto

---
+++ src/screens/Groups/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const Groups: React.FC = () => {
+  return (
+    <View>
+      <Text>Hello There</Text>
+    </View>
+  )
+}
+
+export default Groups
