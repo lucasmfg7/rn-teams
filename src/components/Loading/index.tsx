f96b179f7486f772f5601a2addc55d06bc6df3e2
Lucas Fernandes <lucas_matheus22@hotmail.com>
2023-09-21 13:11:04 -0300

    Componente de Loading

---
+++ src/components/Loading/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Container, LoadIndicator } from './styles'
+
+export const Loading = () => {
+  return (
+    <Container>
+      <LoadIndicator />
+    </Container>
+  )
+}
